2ead407e8110462a210e9e24721bae3e79adae03
Devavrt1234 <95224039+Devavrt1234@users.noreply.github.com>
2023-12-25 01:15:07 +0530

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,14 @@
+import {View,Text} from 'react-native';
+
+const modal=()=>{
+
+    return (
+        <View>
+            <Text>
+          
+            </Text>
+        </View>
+    );
+};
+
+export default modal;
